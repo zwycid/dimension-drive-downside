46fd83706f1f6b1ed591bde79b72ce7d7a13444c
ssidang <ssidang@hotmail.com>
2012-07-03 01:46:10 +0900

    working copy; intermediate state

--- branches/FullScreenSample/src/kr/ssidang/android/MainActivity.java
+++ branches/FullScreenSample/src/kr/ssidang/android/MainActivity.java
@@ -1,5 +1,9 @@
 package kr.ssidang.android;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -9,6 +13,8 @@ import android.view.WindowManager;
 import android.support.v4.app.NavUtils;
 
 public class MainActivity extends Activity {
+	private SensorManager sensorManager;
+	private Sensor oriSensor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,25 @@ public class MainActivity extends Activity {
         // 를 참고. (activity에 있음)
         
         setContentView(R.layout.activity_main);
+        
+        // 방향 센서 가져옴
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        oriSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+//        sensorManager.getOrientation(R, values)
+        sensorManager.registerListener(new OrientationListener(), oriSensor,
+        		SensorManager.SENSOR_DELAY_GAME);
+    }
+    
+    private class OrientationListener implements SensorEventListener {
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+			
+		}
+
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			
+		}
     }
 
     
