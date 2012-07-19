59b76050fd4ae057def7ad8a7b29bb2dd697a6d1
진협 김 <zeraf29@gmail.com>
2012-07-19 20:49:11 +0900

    로고 이미지 적용했습니다. mju.summer2012.itproject.team3.ik.sailingtext 내에 새로 Splash 라는 클래스를 추가 및 splash.xml을 추가했고 manifest에서 splash에 대한 액티비티 추가 및 런처와 액션.메인을 스플래쉬에 주셔야 합니다. 배경 디자인은 임의로 작업한 것이므로 언제든지 바꾸실수 있습니다.(이미지 파일명이 loading 입니다.)

---
+++ zeraf29/Sailing Text/src/mju/summer2012/itproject/team3/lk/sailingtext/Splash.java
@@ -0,0 +1,24 @@
+package mju.summer2012.itproject.team3.lk.sailingtext;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends Activity{
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		Handler h = new Handler();
+		h.postDelayed(new splashhandler(), 3000);
+	}
+	class splashhandler implements Runnable{
+		public void run(){
+			System.out.println(111);
+			startActivity(
+					new Intent(getApplication(),MainActivity.class));
+				Splash.this.finish();
+		}
+		
+	}
+}
